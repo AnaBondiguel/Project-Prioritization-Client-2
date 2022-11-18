3eb6d5d4ea27dda3c7c8b9f42c749d9c81f5f478
Chen <97513965+OrangeKami@users.noreply.github.com>
2022-11-19 09:55:38 +1100

    condition on search page

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useGlobalState } from "../utils/StateContext";
 import {TicketTable} from "./tickets/TicketTable.js";
-import {Stack, Typography, Container} from "@mui/material"
+import {Stack, Typography, Container, Paper} from "@mui/material"
 
   function SearchResults(){
     const {store} = useGlobalState();
@@ -9,19 +9,37 @@ import {Stack, Typography, Container} from "@mui/material"
 
     return (
       <div>
-        <Container>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          mb={5}
-          mt={5}
-        >
-          <Typography variant="h4" gutterBottom>
-            Search Results
-          </Typography>
-        </Stack>
-        <TicketTable tickets={filteredTickets} />
+        <Container> 
+         
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb={5}
+            mt={5}
+          >
+           
+           
+            <Typography variant="h4" gutterBottom>
+              Search Results
+            </Typography>
+          </Stack> 
+          {!filteredTickets.length > 0 ? 
+          (<Paper
+              sx={{
+                textAlign: "center",
+              }}
+            >
+              <Typography variant="h6" paragraph>
+                Not found
+              </Typography>
+
+              <Typography variant="body2">
+                No results found 
+                <br /> Try checking for typos or using complete words.
+              </Typography>
+            </Paper> ):
+          (<TicketTable tickets={filteredTickets} />)}
         </Container>
       </div>
     );
