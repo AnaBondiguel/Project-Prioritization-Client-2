b8b3f75787413266f53ac8dd23bfd39e9d6067a2
AnaBondiguel <michaelyangyang@hotmail.com>
2022-11-20 01:25:37 +1100

    installed cypress testing & added signInandsignUp_spec.cy.js

--- tests/signin.test.js
+++ tests/signin.test.js
@@ -1,23 +1,31 @@
 import {render, screen} from '@testing-library/react';
-import '@testing-library/jest-dom';
-import axios from 'axios';
-import SignIn from '../src/components/SignIn';
+// import '@testing-library/jest-dom';
+// import axios from 'axios';
+// import SigninForm from '../src/components/userpage/SigninForm';
+// import {login} from '../src/services/authServices';
+import React from 'react';
+import SingIn from '../src/pages/SignIn'
 
-// Mock jest and set the type
-jest.mock('axios');
+jest.mock('../src/components/userpage/SigninForm', () => {
+    return <></>
+  })
 
-test('loads and displays greeting', async () => {
-  axios.post.mockResolvedValueOnce({
-    user: {},
-    token: 'some string',
-  });
-
-  // ARRANGE
-  render(<SignIn />)
-
-  // ACT
-  console.log(screen.getByText('Sign In'))
-
-  // ASSERT
+  test('loads and displays greeting', async () => {
+    // axios.post.mockResolvedValueOnce({
+    //   user: {},
+    //   token: 'some string',
+    // });
   
-})
+    // ARRANGE
+    render(
+       
+            <SingIn/>
+    
+          )
+  
+    // ACT
+    console.log(screen.getByText('Sign In'))
+  
+    // ASSERT
+    
+  })
