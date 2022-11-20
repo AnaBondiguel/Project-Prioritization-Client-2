1d14a29570f083ea71f0cb5b52fe0deea6e71acd
Chen <97513965+OrangeKami@users.noreply.github.com>
2022-11-20 14:23:03 +1100

    clean code at navbar config

--- src/components/navbar/config.js
+++ src/components/navbar/config.js
@@ -1,45 +1,27 @@
-// component
-import SvgColor from "../../@mui/components/svgcolor/SvgColor";
-
-// ----------------------------------------------------------------------
-
-const icon = (name) => (
-  <SvgColor
-    src={`/assets/icons/navbar/${name}.svg`}
-    sx={{ width: 1, height: 1 }}
-  />
-);
-
 const navConfig = [
   {
     title: "My Tickets",
     path: "/mytickets",
-    icon: icon("ic_analytics"),
   },
   {
     title: "New Ticket",
     path: "/newticket",
-    icon: icon("ic_user"),
   },
   {
     title: "Listings",
     path: "/listings",
-    icon: icon("ic_cart"),
   },
   {
     title: "blog",
     path: "/blog",
-    icon: icon("ic_blog"),
   },
   {
     title: "Sing In",
     path: "/signin",
-    icon: icon("ic_lock"),
   },
   {
     title: "Not found",
     path: "/404",
-    icon: icon("ic_disabled"),
   },
 ];
 
